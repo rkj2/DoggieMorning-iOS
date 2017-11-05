eb35d16f8a0d2a1265e1d38e7d701a585d9497fd
Rajive Jain <rajivejain@Rajives-MacBook-Pro.local>
2017-11-04 22:45:40 -0700

    Initial Commit

---
+++ DoggieMorning/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DoggieMorning
+//
+//  Created by Rajive Jain on 11/4/17.
+//  Copyright © 2017 Rajive Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ DoggieMorning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoggieMorning
+//
+//  Created by Rajive Jain on 11/4/17.
+//  Copyright © 2017 Rajive Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
